8db3147fb668ebd43d64b0113d7adb9a92f29c83
dicemaster <dicemaster555@gmail.com>
2017-10-20 10:54:38 +0100

    Created a new function that reads and prints the map matrix onto the game window.

--- MapGenerator.py
+++ MapGenerator.py
@@ -25,30 +25,38 @@ tree_tiles = list_of_tiles[15:22]
 water_tile = list_of_tiles[23]
 grass_tiles = list_of_tiles[24:25]
 
-def ReadAndPrintMapMatrix():
-    for row_num, row_list in enumerate(mapMatrix):
-        for tile_num in enumerate(row_list):
-            return tile_num[1]
 """
-the map generator, this will take a random image from
-the tiles list and blit it in the game window every 64 pixels on the x and y axis
+The Old Random map generator, this will take a random image from the tiles
+list and blit it in the game window every 64 pixels on the x and y axis.
 """
 def GenerateRandomMap():
     for x in range(0,game_window_width,64):
         for y in range(0,game_window_height,64):
             randomr = random.randrange(25)
-            number = ReadAndPrintMapMatrix()
-            load_tile = pygame.image.load(list_of_tiles[number])
+            load_tile = pygame.image.load(list_of_tiles[randomr])
             GameWindow.blit(load_tile, (x,y))
 
-GenerateRandomMap()
-print list_of_tiles
+"""
+New map generator, this one works by using a matrix to define where it should place the
+tiles on the game window.
+"""
+def ReadAndPrintMapMatrix():
+    Pos_X = 0
+    Pos_Y = 0
+    for row_num, row_list in enumerate(mapMatrix):
+        for tile_num in enumerate(row_list):
+            load_tile = pygame.image.load(list_of_tiles[tile_num[1]])
+            GameWindow.blit(load_tile, (Pos_X, Pos_Y))
+            Pos_X += 64
+        Pos_Y += 64
+        Pos_X = 0
+
+#GenerateRandomMap()
+
 print mapMatrix
-print mapMatrix[4,0]
-#ReadAndPrintMapMatrix()
+ReadAndPrintMapMatrix()
 
-
-#the game window while loop
+#The game window while loop
 Running = True
 while Running:
     for event in pygame.event.get():
@@ -59,8 +67,5 @@ while Running:
 
     #blits random tiles to the gamewindow on click
     #if event.type == pygame.MOUSEBUTTONDOWN:
-        #GenerateRandomMap()
+        #ReadAndPrintMapMatrix()
     pygame.display.flip()
-
-
-    #CREATE A FOR LOOP THAT TAKES A RANDOM NUMBER (25) AND GIVE IT A POSITION FROM 0 - MAP SIZE X AND Y
