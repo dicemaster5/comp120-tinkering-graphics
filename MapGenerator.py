73ba31bb7c065f0f474aa7ce61c1aa2f26fe8c95
dicemaster <dicemaster555@gmail.com>
2017-10-24 11:51:57 +0100

    Reformatted and changed code to better fit the PEP 8 standards

--- MapGenerator.py
+++ MapGenerator.py
@@ -3,96 +3,99 @@ import random
 import numpy
 import pygame
 
-#Variables to set the width and height of the game window
+# Variables to set the width and height of the game window
 game_window_width = 64 * 20
 game_window_height = 64 * 12
 
-#The game window itself
-GameWindow = pygame.display.set_mode((game_window_width,game_window_height))
+# The game window itself
+game_window = pygame.display.set_mode((game_window_width, game_window_height))
 
-#Array for the tiles list in the tiles folder
+# Array for the tiles list in the tiles folder
 list_of_tiles = []
 
-#the map matrix array
-map_Matrix = []
-
-#for loop to take each tile image from the tiles folder and put them in the tile list
+# takes each tile image from the tiles folder and put them in the tile list
 for root, dirs, files in os.walk('TilesToBeUsed'):
     for file in files:
         if file.endswith('png'):
-            list_of_tiles.append(os.path.join(root,file))
+            list_of_tiles.append(os.path.join(root, file))
 
 """
-New map generator, this one works by using a matrix to define where it should place the
-tiles on the game window.
+New map generator, this one works by using a matrix to define
+where it should place the tiles on the game window.
 """
-def GenerateRandomMap():
-    #Position X and Y on the game window
-    Pos_X = 0
-    Pos_Y = 0
 
-    #This creates a random map matrix using the numpy array library
-    map_Matrix = numpy.random.randint(11, size=(game_window_height / 64, game_window_width / 64))
+
+def generate_random_map():
+    # Position X and Y on the game window
+    pos_x = 0
+    pos_y = 0
+
+    # This creates a random map matrix using the numpy array library
+    map_matrix = numpy.random.randint(11, size=(game_window_height / 64, game_window_width / 64))
 
     """
     The Tile rules.
     This works by checking the tile above and to the left of 
     the current tile and then deciding on what tile to place down
     """
-    for row_num, row_list in enumerate(map_Matrix):
+    for row_num, row_list in enumerate(map_matrix):
         for tile_num in enumerate(row_list):
 
-            connecting_Top_Tiles = [1, 3, 4]
-            top_Connector_Tiles = [1, 5]
+            connecting_top_tiles = [1, 3, 4]
+            top_connector_tiles = [1, 5]
 
-            connecting_Left_Tiles = [2, 3, 5]
-            left_Connector_Tiles = [2, 4]
+            connecting_left_tiles = [2, 3, 5]
+            left_connector_tiles = [2, 4]
 
-            neutral_Tiles = [0, 7, 8, 3, 9, 10]
+            neutral_tiles = [0, 7, 8, 3, 9, 10]
 
-            tile_Above = (row_num -1, tile_num[0])
-            tile_Left = (row_num,tile_num[0] -1)
-            curent_Pos = (row_num,tile_num[0])
+            tile_above = (row_num - 1, tile_num[0])
+            tile_left = (row_num, tile_num[0] - 1)
+            current_pos = (row_num, tile_num[0])
 
-            if map_Matrix.item(tile_Above) in connecting_Top_Tiles and map_Matrix.item(tile_Left) in connecting_Left_Tiles:
-                map_Matrix.itemset(curent_Pos, 6)
+            if map_matrix.item(tile_above) in connecting_top_tiles \
+                    and map_matrix.item(tile_left) in connecting_left_tiles:
+                map_matrix.itemset(current_pos, 6)
 
-            elif map_Matrix.item(tile_Above) not in connecting_Top_Tiles and map_Matrix.item(tile_Left) in connecting_Left_Tiles:
-                map_Matrix.itemset(curent_Pos, random.choice(left_Connector_Tiles))
+            elif map_matrix.item(tile_above) not in connecting_top_tiles \
+                    and map_matrix.item(tile_left) in connecting_left_tiles:
+                map_matrix.itemset(current_pos, random.choice(left_connector_tiles))
 
-            elif map_Matrix.item(tile_Above) in connecting_Top_Tiles and map_Matrix.item(tile_Left) not in connecting_Left_Tiles:
-                map_Matrix.itemset(curent_Pos, random.choice(top_Connector_Tiles))
+            elif map_matrix.item(tile_above) in connecting_top_tiles\
+                    and map_matrix.item(tile_left) not in connecting_left_tiles:
+                map_matrix.itemset(current_pos, random.choice(top_connector_tiles))
 
             else:
-                map_Matrix.itemset(curent_Pos, random.choice(neutral_Tiles))
+                map_matrix.itemset(current_pos, random.choice(neutral_tiles))
 
-    #for loop that blits to the screen each tile number within the modified ruled matrix
-    for row_num, row_list in enumerate(map_Matrix):
+    # for loop that blits to the screen each tile number within the modified ruled matrix
+    for row_num, row_list in enumerate(map_matrix):
         for tile_num in enumerate(row_list):
             load_tile = pygame.image.load(list_of_tiles[tile_num[1]])
-            GameWindow.blit(load_tile, (Pos_X, Pos_Y))
+            game_window.blit(load_tile, (pos_x, pos_y))
 
-            Pos_X += 64
-        Pos_Y += 64
-        Pos_X = 0
+            pos_x += 64
+        pos_y += 64
+        pos_x = 0
 
-    #this will print the matrix in the console
-    #print map_Matrix
+    # this will print the matrix in the console
+    print map_matrix
 
-#calls the
-GenerateRandomMap()
 
-#The game window while loop
-Running = True
-while Running:
+# Generates a random map with a set of rules for paths to stay aligned
+generate_random_map()
+
+# The game window while loop
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            Running = False
+            running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            Running = False
+            running = False
 
-    #blits random tiles to the gamewindow on mouse click down
+    # blits random tiles to the gamewindow on mouse click down
     if event.type == pygame.MOUSEBUTTONDOWN:
-        GenerateRandomMap()
+        generate_random_map()
 
     pygame.display.flip()
